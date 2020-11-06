63f4969e14a96419c075eba6f7e57410bf5dd00b
Drei Rhondov <63360808+drearondov@users.noreply.github.com>
2020-11-06 11:23:38 -0500

    :construction: Start screen on start

--- app/views/startscreen/startscreen.py
+++ app/views/startscreen/startscreen.py
@@ -1,29 +1,5 @@
-from kivy.clock import Clock
-from kivy.storage.jsonstore import JsonStore
 from kivy.uix.screenmanager import Screen
 
-from app.views.welcomescreen.welcomescreen import WelcomeScreen
-from app.views.homescreen.homescreen import HomeScreen
-
-from library.settings import local_user
 
 class StartScreen(Screen):
-
-    def __init__(self, **kwargs):
-        super(Screen, self).__init__(**kwargs)
-    
-    def fetch_local_user(self):
-        """
-        Find if there is a local user.
-        """
-        local_store = JsonStore('library/local_user.json')
-        local_user.local_ID = local_store.get('local_user')['local_ID']
-
-        if local_user.local_ID != "":
-            self.manager.current = 'home_screen'
-            local_user.get_user_data(local_user.local_ID)
-            self.manager.ids['home_screen'].show_date()
-            self.manager.ids['home_screen'].show_accounts()
-            #self.manager.ids['home_screen'].show_transactions()
-        else:
-            self.manager.current = 'welcome_screen'
+    pass
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ['KIVY_IMAGE'] = 'sdl2'
 
 import kivy
@@ -10,20 +11,34 @@ Config.set('graphics', 'height', '812')
 
 from kivy.app import App
 from kivy.storage.jsonstore import JsonStore
-from kivy.uix.screenmanager import ScreenManager
 
 from app.views.startscreen.startscreen import StartScreen
+from app.views.welcomescreen.welcomescreen import WelcomeScreen
+from app.views.homescreen.homescreen import HomeScreen
+
 from library.settings import local_user
 
-
-class Manager(ScreenManager):
-    pass
 
 class MainApp(App):
 
     def build(self):
         self.title = "moneyBox"
-        return Manager()
+
+    def on_start(self):
+
+        self.root.current = 'start_screen'
+
+        local_store = JsonStore('library/local_user.json')
+        local_user.local_ID = local_store.get('local_user')['local_ID']
+
+        if local_user.local_ID != "":
+            self.root.current = 'home_screen'
+            local_user.get_user_data(local_user.local_ID)
+            self.root.ids['home_screen'].show_date()
+            self.root.ids['home_screen'].show_accounts()
+            #self.root.ids['home_screen'].show_transactions()
+        else:
+            self.root.current = 'welcome_screen'
 
     def on_stop(self):
         if local_user.local_ID == "":
